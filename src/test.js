d4a74f9c09fc8f2373280020d9487efa9cc6d31c
saturn597 <saturn5971@gmail.com>
2017-06-11 17:26:30 -0400

    Generalized GIF "tests"
    Created a react component that takes a bunch of image sources and makes
    a GIF out of them. This is just to simplify the display of
    examples/tests.

--- src/test.js
+++ src/test.js
@@ -6,29 +6,114 @@ import {Frame, getGifUrl} from './gifs.js';
 require('babel-polyfill');
 
 
+function getImage(src) {
+    // Return a promise to load an image - useful for preloading.
+
+    return new Promise((resolve, reject) => {
+        const i = new Image();
+        i.onload = resolve;
+        i.onerror = reject;
+        i.src = src;
+    });
+}
+
+
 class Test extends React.Component {
     render() {
-        const animCanvas = [];
-        const animImg = [];
-        const animCtx = [];
-        let testFrames = [];
-        for (let i = 0; i < 3; i++) {
-            animCanvas[i] = document.createElement('canvas');
-            animImg[i] = document.getElementById('animation' + (i + 1));
-            animCanvas[i].width = animImg[i].width;
-            animCanvas[i].height = animImg[i].height;
-            animCtx[i] = animCanvas[i].getContext('2d');
-            animCtx[i].drawImage(animImg[i], 0, 0);
-            testFrames[i] = new Frame(animCanvas[i], Math.floor(Math.random() * 100 + 1), 1);
-        }
-        testFrames = [testFrames[1], testFrames[2], testFrames[1], testFrames[2], testFrames[0]];
+        const basicGif = {
+            desc: 'Basic GIF.',
+            frames: [{src: 'data:image/gif;base64,R0lGODlhCgAKAJEAAP////8AAAAA/wAAACH5BAAAAAAALAAAAAAKAAoAAAIWjC2Zhyoc3DOgAnXslfqo3mCMBJFMAQA7'}],
+        };
 
-        const data = getGifUrl(testFrames, 0);
-        return <img src={getGifUrl(testFrames, 0)} />
+        const basicAnimation = {
+            desc: 'Basic animation.',
+            frames: [
+                {src: 'animation1.gif', delay: 1},
+                {src: 'animation2.gif', delay: 1},
+                {src: 'animation3.gif', delay: 1},
+            ],
+        };
+
+        const tests = [basicGif, basicAnimation];
+
+        const testers = tests.map((t, k) =>
+                <GifTester
+                    key={k}
+                    frames={t.frames}
+                    desc={t.desc}
+                    show={true} />);
+
+        return (
+            <div>
+                Legend:
+                <div className="test">
+                    Description.
+                    <div className="testImages">Base image(s)</div>
+                    <div className="testGif">Gif created</div>
+                </div>
+                Tests: {testers}
+            </div>
+        )
     }
 }
 
-window.addEventListener('load', function() {
+
+class GifTester extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {imagesLoaded: false};
+        const promises = this.props.frames.map((i) => getImage(i.src));
+        Promise.all(promises).then(() => this.setState({imagesLoaded: true}));
+
+        this.gifSrc = '';
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        if (nextState.imagesLoaded) {
+            const frames = nextProps.frames.map((image) => {
+                const i = new Image();
+                i.src = image.src;
+
+                const c = document.createElement('canvas');
+                c.width = i.width;
+                c.height = i.height;
+
+                const ctx = c.getContext('2d');
+                ctx.drawImage(i, 0, 0);
+
+                return new Frame(c, image.delay || 50, 1);
+            });
+            this.gifSrc = getGifUrl(frames, 0);
+        }
+    }
+
+    render() {
+        if (!this.props.show) {
+            return <div />
+        }
+
+        const images = this.props.frames.map((i, k) =>
+            <img src={i.src} key={k} />);
+        if (this.state.imagesLoaded) {
+            return (
+                <div className="test">
+                    {this.props.desc}
+                    <div className="testImages">
+                        {images}
+                    </div>
+                    <div className="testGif">
+                        <img src={this.gifSrc} />
+                    </div>
+                </div>
+            );
+        } else {
+            return <div className="test">{images}</div>;
+        }
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
             <Test />,
             document.getElementById('mount')
