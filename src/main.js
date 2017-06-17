11abf9e3135ffe497e3020a6d147cc0b4b9476dc
saturn597 <saturn5971@gmail.com>
2017-06-17 15:44:15 -0400

    Add frame and remove frame buttons
    In the course of doing this, added an indication of which frame is
    selected.
    
    Also, split out a method (newFrameData) to create blank frame data,
    since I'm now doing this in multiple places.

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
             <GifEditor
                 defaultDelay={1}
+                defaultDisposal={1}
                 initialFrameCount={3}
 
                 width={700}
@@ -23,10 +24,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
 class FrameInfo extends React.Component {
     render() {
-        return <div className="frameInfo" onClick={this.props.onClick}>
-            Duration: {this.props.frame.delay}
-            <img src={this.props.frame.canvas.toDataURL()} width="100" height="100" />
-        </div>;
+        const id = this.props.selected ? 'selected' : null;
+        const removeFrame = (e) => {
+            e.stopPropagation();
+            this.props.removeFrame();
+        };
+
+        return (
+            <div className="frameInfo"
+                id={id}
+                onClick={this.props.selectFrame}>
+
+                Duration: {this.props.frame.delay}
+                <img src={this.props.frame.canvas.toDataURL()}
+                    width="100"
+                    height="100" />
+                <button onClick={removeFrame}>
+                    Remove Frame
+                </button>
+            </div>
+        );
     }
 }
 
@@ -36,36 +53,7 @@ class GifEditor extends React.Component {
 
         const frameData = [];
         for (let i = 0; i < this.props.initialFrameCount; i++) {
-            const c = document.createElement('canvas');
-            c.width = this.props.width;
-            c.height = this.props.height;
-
-            // TODO: For now, we'll fill everything in with a background color,
-            // so that there are no transparent areas.  We COULD allow
-            // transparency in the GIFs.  But there are a couple of problems
-            // for now:
-            //
-            // 1) Animating with a disposal method of "1" means that new frames
-            // just get "added" to the last one, so if there's transparency you
-            // can still see the old frame underneath, which doesn't seem
-            // right. This might be fixable by using a disposal method of 2
-            // (which restores to background color after the frame) and setting
-            // a background color that's also transparent. This will require
-            // adding a global color table, since the background color comes
-            // from the global color table.
-            //
-            // 2) The lines we're drawing on canvas are "fuzzy", containing
-            // areas of partial but not complete transparency. GIFs appear to
-            // only support pixels that are either opaque or transparent, not
-            // in between.
-            const ctx = c.getContext('2d');
-            ctx.fillStyle = 'white';
-            ctx.fillRect(0, 0, c.width, c.height);
-
-            frameData.push({
-                canvas: c,
-                delay: this.props.defaultDelay,
-                disposal: 1});
+            frameData.push(this.newFrameData());
         }
 
         this.state = {
@@ -74,8 +62,15 @@ class GifEditor extends React.Component {
             gifData: ''
         };
 
+        this.addFrame = this.addFrame.bind(this);
         this.drawingUpdated = this.drawingUpdated.bind(this);
         this.updateGif = this.updateGif.bind(this);
+    }
+
+    addFrame() {
+        this.setState((state) => ({
+            frameData: state.frameData.concat(this.newFrameData())
+        }));
     }
 
     drawingUpdated(newCanvas) {
@@ -89,6 +84,57 @@ class GifEditor extends React.Component {
         this.setState({frameData: newFrameData});
     }
 
+    newFrameData() {
+        const c = document.createElement('canvas');
+        c.width = this.props.width;
+        c.height = this.props.height;
+
+        // TODO: For now, we'll fill everything in with a background color, so
+        // that there are no transparent areas.  We COULD allow transparency in
+        // the GIFs.  But there are a couple of problems for now:
+        //
+        // 1) Animating with a disposal method of "1" means that new frames
+        // just get "added" to the last one, so if there's transparency you can
+        // still see the old frame underneath, which doesn't seem right. This
+        // might be fixable by using a disposal method of 2 (which restores to
+        // background color after the frame) and setting a background color
+        // that's also transparent. This will require adding a global color
+        // table, since the background color comes from the global color table.
+        //
+        // 2) The lines we're drawing on canvas are "fuzzy", containing areas
+        // of partial but not complete transparency. GIFs appear to only
+        // support pixels that are either opaque or transparent, not in
+        // between. By filling everything up with white to begin with, we
+        // prevent any areas of the canvas from having partial transparency.
+        const ctx = c.getContext('2d');
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, c.width, c.height);
+
+        return {
+            canvas: c,
+            delay: this.props.defaultDelay,
+            disposal: this.props.defaultDisposal,
+        };
+    }
+
+    removeFrame(k) {
+        this.setState((state) => {
+            if (state.frameData.length === 1) {
+                return;
+            }
+
+            let currentFrame = state.currentFrame;
+            if (k <= currentFrame && currentFrame > 0) {
+                currentFrame -= 1;
+            }
+
+            return {
+                frameData: update(state.frameData, {$splice: [[k, 1]]}),
+                currentFrame,
+            };
+        });
+    }
+
     updateGif() {
         const frames = this.state.frameData.map((f) =>
                 new Frame(f.canvas, f.delay, f.disposal));
@@ -99,17 +145,21 @@ class GifEditor extends React.Component {
     }
 
     render() {
+        const currentFrame = this.state.frameData[this.state.currentFrame];
+
         const frameDisplay = [];
         let i = 0;
         for (let f of this.state.frameData) {
+            const frameNum = i;
             frameDisplay.push(<FrameInfo
                     frame={f}
                     key={i}
-                    onClick={((i) => this.setState({currentFrame: i})).bind(this, i)} />);
+                    selectFrame={() => this.setState({currentFrame: frameNum})}
+                    removeFrame={() => this.removeFrame(frameNum)}
+                    selected={f === currentFrame} />);
             i++;
         }
 
-        const currentFrame = this.state.frameData[this.state.currentFrame];
         return <div>
             <DrawCanvas
                 drawingUpdated={this.drawingUpdated}
@@ -117,6 +167,7 @@ class GifEditor extends React.Component {
                 width={this.props.width}
                 height={this.props.height} />
             {frameDisplay}
+            <button onClick={this.addFrame}>Add frame</button>
             <button onClick={this.updateGif}>Update GIF</button>
             <img src={this.state.gifData} />
         </div>;
